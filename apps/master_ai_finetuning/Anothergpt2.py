9823895d6f9d4fa952f15e2cb41e49c60046c893
Sahil Sarkar <mail.analsarkar@gmail.com>
2023-05-25 02:35:09 +0530

    A

--- apps/master_ai_finetuning/Anothergpt2.py
+++ apps/master_ai_finetuning/Anothergpt2.py
@@ -6,7 +6,6 @@ from torch.utils.data import DataLoader
 from transformers import AutoModelForCausalLM, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling, Trainer, TrainingArguments
 
 
-from initialize import init
 from master_ai_finetuning.AnotherCustomDataSet import CustomTextDataset
 
 #https://github.com/huggingface/notebooks/blob/main/examples/language_modeling.ipynb
@@ -14,13 +13,13 @@ from master_ai_finetuning.AnotherCustomDataSet import CustomTextDataset
 # start a new wandb run to track this script
 wandb.init(
     # set the wandb project where this run will be logged
-    project="my-pytorch-project",
+    project="AnotherGPT",
 
     # track hyperparameters and run metadata
     config={
         "learning_rate": 1e-5,
-        "architecture": "distilgpt2",
-        "epochs": 10,
+        "architecture": "gpt2-medium",
+        "epochs": 50,
         "max_length":1024,
         "batch_size":8
     }
@@ -29,8 +28,7 @@ wandb.init(
 train_path = 'train_desi_dish_dataset.text'
 test_path = 'test_desi_dish_dataset.text'
 
-model_name = "distilgpt2"
-model_name = "distilgpt2_1"
+model_name = "gpt2-medium"
 
 # Load pre-trained GPT-2 model and tokenizer
 model = AutoModelForCausalLM.from_pretrained(model_name)
@@ -44,26 +42,6 @@ batch_size = 8
 train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 eval_data_loader = DataLoader(eval_dataset, batch_size=batch_size, shuffle=False)
 
-
-# # Load training data as TextDataset
-# train_dataset = TextDataset(
-#     tokenizer=tokenizer,
-#     file_path=train_path, # path to training data file
-#     block_size=128 # maximum sequence length
-# )
-#
-# # Load validation data as TextDataset
-# eval_dataset = TextDataset(
-#     tokenizer=tokenizer,
-#     file_path=test_path, # path to validation data file
-#     block_size=128 # maximum sequence length
-# )
-#
-# # Define data collator for language modeling
-# data_collator = DataCollatorForLanguageModeling(
-#     tokenizer=tokenizer, mlm=False
-# )
-
 # Define training arguments
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -71,6 +49,10 @@ model.to(device)
 
 # Set optimizer and learning rate
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
+
+# Set the number of iterations after which to save a checkpoint
+checkpoint_interval = 100
+iteration = 0
 
 # Training loop
 num_epochs = 50
@@ -91,6 +73,14 @@ for epoch in range(num_epochs):
 
         total_loss += loss.item()
 
+        iteration += 1
+
+        # Save a checkpoint every checkpoint_interval iterations
+        if iteration % checkpoint_interval == 0:
+            checkpoint_path = os.environ["GENERATED_MODEL"] + f"/"+model_name+"/checkpoint_{iteration}.pt"
+            torch.save(model.state_dict(), checkpoint_path)
+            print(f"Checkpoint saved at iteration {iteration}")
+
     # Print average loss for the epoch
     average_loss = total_loss / len(train_data_loader)
     print(f"Epoch {epoch + 1}/{num_epochs}, Average Loss: {average_loss:.4f}")
@@ -100,6 +90,7 @@ for epoch in range(num_epochs):
     # Evaluation
     model.eval()
     total_eval_loss = 0
+    total_tokens = 0
     with torch.no_grad():
         for batch in eval_data_loader:
             input_ids = batch['input_ids'].to(device)
@@ -110,30 +101,19 @@ for epoch in range(num_epochs):
             loss = outputs.loss
 
             total_eval_loss += loss.item()
+            total_tokens += torch.numel(input_ids["input_ids"])
 
     # Print average evaluation loss for the epoch
     average_eval_loss = total_eval_loss / len(eval_data_loader)
+    perplexity = torch.exp(total_loss / total_tokens)
     print(f"Epoch {epoch + 1}/{num_epochs}, Average Evaluation Loss: {average_eval_loss:.4f}")
+    print(f"Epoch {epoch + 1}/{num_epochs}, Perplexity: {perplexity:.4f}")
     wandb.log({'epoch': epoch + 1, "eval_loss": average_loss})
-
-# Define a function to compute perplexity during evaluation
-# def compute_perplexity(eval_pred):
-#     logits, labels = eval_pred.predictions, eval_pred.label_ids
-#     loss = trainer.compute_loss(eval_pred)
-#     perplexity = loss.exp()
-#     return {'perplexity': perplexity}
-#
-# # Set the compute_metrics argument in Trainer to use the perplexity function
-# trainer.compute_metrics = compute_perplexity
-#
-# # Evaluate the model and calculate perplexity
-# evaluation_result = trainer.evaluate()
-#
-# print("Perplexity:", evaluation_result["perplexity"])
+    wandb.log({'epoch': epoch + 1, "perplexity": perplexity})
 
 
 #Save the model
-model.save_pretrained(os.environ["GENERATED_MODEL"]+'/../../distilgpt2_2')
-tokenizer.save_pretrained(os.environ["GENERATED_MODEL"]+'/../../distilgpt2_2')
+model.save_pretrained(os.environ["GENERATED_MODEL"]+model_name+"_"+num_epochs)
+tokenizer.save_pretrained(os.environ["GENERATED_MODEL"]+model_name+"_"+num_epochs)
 
 wandb.finish()
